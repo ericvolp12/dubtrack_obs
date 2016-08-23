8f14c0c368cf4321501b9d23dc00194dfa04b8c6
Eric Volpert <ericvolp12@gmail.com>
2016-08-23 14:40:09 -0400

    Built the thing.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var dubtrack = reqiure('./modules/dubtrack');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index');
+});
+
+router.get('/room/:roomID', function (req, res) {
+    if (req.params.roomID) {
+        dubtrack.getRoomNowPlaying(req.params.roomID).then(function(songName){
+            res.render('nowPlaying', {songName:songName});
+        }, function(err){
+            res.status(404).send(err);
+        })
+    } else {
+        res.status(400).send("Please specify a room ID!");
+    }
 });
 
 module.exports = router;
---
+++ routes/modules/dubtrack.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Eric Volpert on 8/23/2016.
+ */
+https = require("https");
+let apiBASE = "https://api.dubtrack.fm";
+
+function getRoomNowPlaying(roomID) {
+    return new Promise(function (resolve, reject) {
+        let apiPATH = "/room/";
+        apiPATH += roomID;
+        https.get(apiBASE + apiPATH, function (res) {
+            var body = '';
+            res.on('data', function(chunk){
+                body += chunk;
+            });
+
+            res.on('end', function(){
+               var apiResponse = JSON.parse(body);
+                if(apiResponse.currentSong.songid) {
+                    console.log("Got response from Dubtrack API, currently playing song is: " + apiResponse.currentSong.songid + " in room: " + roomID);
+                    resolve(apiResponse.currentSong.songid);
+                }else{
+                    console.log("No current song found for room: " + roomID);
+                    reject("No current song found for room: " + roomID);
+                }
+            });
+
+        });
+    });
+}
+
+
+module.exports = {
+    getRoomNowPlaying: getRoomNowPlaying
+};
