e935007173a1be15336a397ace57829554e21b11
Eric Volpert <ericvolp12@gmail.com>
2016-08-23 15:21:46 -0400

    Websockets, pls work?

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var dubtrack = require("routes/modules/dubtrack");
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+app.io = io;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 'use strict';
-var express = require('express');
-var router = express.Router();
+var app = require('express');
+var router = app.Router();
 var dubtrack = require('./modules/dubtrack');
 
 
@@ -12,7 +12,13 @@ router.get('/', function (req, res, next) {
 router.get('/room/:roomID', function (req, res) {
     if (req.params.roomID) {
         dubtrack.getRoomNowPlaying(req.params.roomID).then(function(songName){
+            setTimeout(dubtrack.getRoomNowPlaying.then(function(songName){
+                app.io.emit('songName', {songName});
+            }, function(err){
+                app.io.emit('songName', {err});
+            }), 3000);
             res.render('nowPlaying', {songName:songName});
+
         }, function(err){
             res.status(404).send(err);
         })
@@ -20,5 +26,7 @@ router.get('/room/:roomID', function (req, res) {
         res.status(400).send("Please specify a room ID!");
     }
 });
+
+
 
 module.exports = router;
